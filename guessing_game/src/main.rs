cc079def7e5d0623db77f23cbff346c6895a84f0
John Hume <duelin.markers@gmail.com>
2014-08-06 23:48:32 +0100

    Add some guessing_game tests

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::rand;
 
+#[allow(dead_code)]
 fn main() {
     println!("Guess the number!");
 
@@ -39,4 +40,23 @@ fn cmp(a: uint, b: uint) -> Ordering {
     if a < b { Less }
     else if a > b { Greater }
     else { Equal }
+}
+
+#[cfg(test)]
+mod cmp {
+
+    #[test]
+    fn when_first_is_less_than_second() {
+        assert_eq!(Less, super::cmp(0, 1));
+    }
+
+    #[test]
+    fn when_args_are_equal() {
+        assert_eq!(Equal, super::cmp(1, 1));
+    }
+
+    #[test]
+    fn when_first_is_greater_than_second() {
+        assert_eq!(Greater, super::cmp(1, 0));
+    }
 }
