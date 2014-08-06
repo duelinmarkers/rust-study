687dc7e5e4f438a1045e8dbb191b883c613b8004
John Hume <duelin.markers@gmail.com>
2014-08-06 22:55:53 +0100

    guessing_game step 2: rand::random

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,19 @@
 use std::io;
+use std::rand;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = (rand::random::<int>() % 100i) + 1i;
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
     let input = io::stdin().read_line()
                            .ok()
                            .expect("Failed to read line");
+
 
     println!("You guessed: {}", input);
 }
