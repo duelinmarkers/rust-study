c040beb0d51a353bd477d1311c206e54f179595f
John Hume <duelin.markers@gmail.com>
2014-08-06 23:05:21 +0100

    guessing_game step 3: parse input, compare guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,7 +13,27 @@ fn main() {
     let input = io::stdin().read_line()
                            .ok()
                            .expect("Failed to read line");
+    let input_num: Option<uint> = from_str(input.as_slice().trim());
 
+    let num = match input_num {
+        Some(num) => num,
+        None      => {
+            println!("Please input a number!");
+            return;
+        }
+    };
 
-    println!("You guessed: {}", input);
+    println!("You guessed: {}", num);
+
+    match cmp(num, secret_number) {
+        Less    => println!("Too small!"),
+        Greater => println!("Too big!"),
+        Equal   => println!("You win!"),
+    }
+}
+
+fn cmp(a: uint, b: uint) -> Ordering {
+    if a < b { Less }
+    else if a > b { Greater }
+    else { Equal }
 }
